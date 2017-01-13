e5e64b14e2e45240ab4c43f05c4f8f38d3dfa983
Avital <avitalbarzelai@gmail.com>
2017-01-13 19:35:35 +0200

    home page buttons visibility

---
+++ src/components/feeling/feeling.vue
@@ -0,0 +1,39 @@
+<template lang="html">
+
+
+  <section class="feeling container below-nav">
+    <h1>feeling Component</h1>
+
+  </section>
+
+</template>
+
+<script lang="js">
+  // import authService from '../../services/auth.service';
+
+  export default  {
+    name: 'feeling',
+    props: [],
+    // beforeRouteEnter ( to, from, next ) {
+    //   authService.protectRoute(next);
+    // },    
+    mounted() {
+
+    },
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+</script>
+
+<style scoped lang="scss">
+ 
+</style>
