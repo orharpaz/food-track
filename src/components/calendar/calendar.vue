ab73fa303bfa732a39c04eb63fc4502dd971d562
orharpaz <orharpaz1@gmail.com>
2017-01-14 17:54:53 +0200

    improve calendar

--- src/components/calendar/calendar.vue
+++ src/components/calendar/calendar.vue
@@ -1,6 +1,6 @@
 <template lang="html">
     <section class="admin container below-nav">
-        <h1>Calendar Component</h1>
+        <h1>Meals Calendar</h1>
         <div class="calendar"></div>
     </section>
 
@@ -22,8 +22,11 @@
                     title: "Banana, Apple",
                     start: moment(1484210059985).format(),
                     end: moment(1484263059985).format(),
-                    // allDay: true
+                    allDay: false,
+                    backgroundColor: "#ff0000"
                 }],
+
+                
 
             }
         },
@@ -42,7 +45,7 @@
                 // hiddenDays: [  4, 5,6 ], //choose which days to hide
                 // hiddenDays: [ 0, 1,2,3 ],
 
-                header: { center: 'month, agendaWeek' }, // buttons for switching between views
+                header: { center: 'month, agendaWeek, listWeek' }, // buttons for switching between views
                 views: {
                     month: { // name of view
                         titleFormat: 'YYYY, MM, DD' // name of view
--- src/main.js
+++ src/main.js
@@ -6,10 +6,7 @@ import Vue from 'vue';
 import store from './store';
 import router from './routes';
 import MainNav from './components/main-nav';
-// import moment from 'moment';
-// import jquery from 'jquery';
-// window.jQuery = jQuery;
-// import FullCalendar from 'fullcalendar';
+
 
 const app = new Vue({
   router,
