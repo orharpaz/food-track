388ee53f7b048a5574f67a103c5549ec4ed1cbd2
orharpaz <orharpaz1@gmail.com>
2017-01-15 18:23:44 +0200

    added footer

--- src/main.js
+++ src/main.js
@@ -6,13 +6,15 @@ import Vue from 'vue';
 import store from './store';
 import router from './routes';
 import MainNav from './components/main-nav';
+import MainFooter from './components/main-footer/index';
 
 
 const app = new Vue({
   router,
   store,
   components: {
-    MainNav
+    MainNav,
+    MainFooter
   }
 }).$mount('#app');
 
