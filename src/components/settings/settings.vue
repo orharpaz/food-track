ae443b274215eb94526aa9a11818187255353ebc
Avital <avitalbarzelai@gmail.com>
2017-01-15 18:26:42 +0200

    start work on settings UI

--- src/components/settings/settings.vue
+++ src/components/settings/settings.vue
@@ -3,7 +3,13 @@
   <section class="admin container below-nav">
     <h1>Settings Component</h1>
     <input v-model="message" placeholder="edit me">
-    <p>Message is: {{ message }}</p>
+      <p>Message is: {{ message }}</p>
+
+    <div class="switch flex justify-center">
+      <p>Send Push Notifications?</p>
+      <input id="cmn-toggle-4" class="cmn-toggle cmn-toggle-round-flat" type="checkbox">
+      <label for="cmn-toggle-4"></label>
+    </div>
   </section>
 
 </template>
@@ -22,7 +28,7 @@
     },
     data() {
       return {
-        message :'yabaloli'
+        message: 'yabaloli'
       }
     },
     methods: {
@@ -35,5 +41,62 @@
 </script>
 
 <style scoped lang="scss">
+
+
+html {
+  background-color: black;
+}
+
+.cmn-toggle {
+  position: absolute;
+  margin-left: -9999px;
+  visibility: hidden;
+}
+
+.cmn-toggle + label {
+  display: block;
+  position: relative;
+  cursor: pointer;
+  outline: none;
+  user-select: none;
+}
+
+input.cmn-toggle-round-flat + label {
+  /* width = 2*height or 2*border-radius */
+  padding: 2px;
+  width: 120px;
+  height: 60px;
+  border: 3px solid #dddddd;
+  border-radius: 60px;
+  transition: border-color 0.4s;
+}
+
+input.cmn-toggle-round-flat + label:before,
+input.cmn-toggle-round-flat + label:after {
+  display: block;
+  position: absolute;
+  content: "";
+}
+
+input.cmn-toggle-round-flat + label:after {
+  /* width = 2*border-radius */
+  top: 4px;
+  left: 4px;
+  bottom: 4px;
+  width: 52px;
+  background-color: #dddddd;
+  border-radius: 52px;
+  transition: margin 0.4s, background 0.4s;
+}
+
+input.cmn-toggle-round-flat:checked + label {
+  border-color: #8ce196;
+}
+
+input.cmn-toggle-round-flat:checked + label:after {
+  /* margin-left = border-radius from 'input.cmn-toggle-round-flat + label' */
+  margin-left: 60px;
+  background-color: #8ce196;
+}
 
 </style>
