e870fd86667dd5dc5eb86cdb943cea6f85376b9e
Avital <avitalbarzelai@gmail.com>
2017-01-17 17:25:16 +0200

    work on responsive nav bar

--- src/components/main-nav/main-nav.component.js
+++ src/components/main-nav/main-nav.component.js
@@ -1,11 +1,28 @@
-import {mapMutations, mapGetters} from 'vuex';
+import { mapMutations, mapGetters } from 'vuex';
 import authService from '../../services/auth.service';
-import {SIGN_OUT} from '../../modules/auth/auth.module';
-import {REMOVE_FROM_CART} from '../../modules/cart/cart.module';
+import { SIGN_OUT } from '../../modules/auth/auth.module';
+import { REMOVE_FROM_CART } from '../../modules/cart/cart.module';
 
 export default {
-  name    : 'main-nav',
-  methods : {
+  name: 'main-nav',
+  data() {
+    return {
+      // windowWidth,
+      isShown: window.innerWidth > 375,
+
+    }
+  },
+  watch:{
+    // windowWidth: windowSize(){
+    //   if ()
+    // } 
+  },
+  methods: {
+    toggle() {
+      this.isShown = !this.isShown;
+      console.log('toggle?');
+      
+    },
     signout() {
       authService.signout();
       this.$store.commit(SIGN_OUT);
@@ -22,10 +39,9 @@ export default {
       'cart'
     ]),
     ...mapGetters({
-        isLoggedIn: 'isLoggedIn',
-        user: 'user'
-      }
+      isLoggedIn: 'isLoggedIn',
+      user: 'user'
+    }
     ),
-  }
-
+  },
 }
