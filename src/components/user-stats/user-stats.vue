da721d3094b66daedf4fc8dc65515a030a147286
Noa <nonistein@gmail.com>
2017-01-18 19:23:14 +0200

    rendering stats

--- src/components/user-stats/user-stats.vue
+++ src/components/user-stats/user-stats.vue
@@ -2,6 +2,15 @@
 
   <section class="admin container below-nav">
     <h1>User statistics Component</h1>
+    <ul>
+      <pre>{{stats}}</pre>
+      <br> allDataFood: {{allDataFood}}
+      <br> allDataStats:{{allDataStats}}
+      <li v-for="currStat in stats">
+        <div>{{...currStat.name}}</div>
+        <div>{{currStat.rating}}</div>
+      </li>
+    </ul>
   </section>
 
 </template>
@@ -10,26 +19,104 @@
 
   export default {
 
-    mounted() {
-      this.$http.get('http://localhost:3003/data/stats').then((res) => {
-        console.log('success with', res.body);
-
-        // success callback
-      }, (err) => {
-        // error callback
-        console.log('error');
-      });
-    },
     data() {
       return {
+
+        stats: {},
+        allDataFood: [],
+        allDataStats: {}
 
       }
     },
     methods: {
-
     },
     computed: {
 
+    },
+    mounted() {
+      this.$http.get('http://localhost:3003/data/stats')
+        .then((res) => res.json())
+        .then((resJson) => {
+          this.stats = resJson;
+
+          //filter all the foods to allDataFood arr
+          this.stats.forEach((currStat) => {
+            currStat.name.forEach((foodName) => {
+              this.allDataFood.push({
+                name: foodName,
+                rate: currStat.rating
+              })
+            });
+          });
+
+          //create table of food and rating
+          this.allDataFood.forEach((foodObj) => {
+            let food = foodObj.name;
+            let newRating = foodObj.rate;
+            let newCount = foodObj.count;
+
+            
+            // food is not in the statistics
+            if (this.allDataStats[food] === undefined) {
+              this.allDataStats[food] = {
+                food: food,
+                rating: newRating,
+                count: 0
+              };
+            
+              // food is already in the stats array => update rating
+            } else {  
+              
+              let currRate = this.allDataStats[food]['rating'];
+console.log('currRate', currRate);
+
+              let currCount = this.allDataStats[food]['count'];
+console.log('currCount', currCount);
+              let newRate = (currRate * currCount + newRating) / (currCount + 1);
+                  console.log('newRate',newRate);
+                  
+              this.allDataStats[food]['rating'] = newRate;
+  console.log('allDataStats', this.allDataStats[food]['rating']);              
+            }
+            this.allDataStats[food]['count']++;
+          });
+              console.log('finalFoodObj', this.allDataStats);
+
+
+
+          //console.log('success with', res.body);
+          // console.log('success with', resJson);
+          // let statsObjArr = res.body
+          // // this.stats = statsObjArr;
+
+          // for (var key in statsObjArr) {
+          //   var obj = statsObjArr[key];
+          //   for (var prop in obj) {
+
+          //     this.stats.rating = obj[prop];
+          //     // this.stats.name = obj[prop]
+          //   }
+          // }
+
+
+
+          // for (var key in statsObjArr) {
+          //   var obj = statsObjArr[key];
+          //   for (var prop in obj) {
+          //     if (obj.hasOwnProperty(prop)) {
+          //       this.stats.push(prop + " = " + obj[prop])
+          //       console.log(prop + " = " + obj[prop]);
+          //     }
+          //   }
+
+          // }
+
+
+          // success callback
+        }, (err) => {
+          // error callback
+          console.log('error');
+        });
     }
   }
 </script>
